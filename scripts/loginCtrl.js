20d6762beb171e6ac71e2c6ca03507c448c273a2
dzeifman <dzeifman2010@gmail.com>
2017-05-18 15:24:41 +0900

    Add redirect functionality to login pg

--- scripts/loginCtrl.js
+++ scripts/loginCtrl.js
@@ -1,3 +1,25 @@
-angular.module('myApp').controller('loginCtrl', ['$scope', 'dataServiceMock', 'stateService', function ($scope, dataServiceMock, stateService) {
-    $scope.login="test123";
+angular.module('myApp').controller('loginCtrl', ['$location', '$scope', 'dataServiceMock', 'stateService', function ($location, $scope, dataServiceMock, stateService) {
+    
+    
+    //models
+    $scope.login = '';
+    $scope.pw = '';
+    //functions
+    $scope.clickRegister = clickRegister;
+    
+    init();
+    function clickRegister () {
+        if ($scope.login.length > 0) {
+            $location.path('register');
+        } else {
+            //display error message
+        }
+    }
+    
+    function init () {
+        if (stateService.loggedIn) {
+            //if we are already logged in, redirect to success page
+            $location.path('/login_success');
+        }
+    }
 }]);
--- scripts/registrationCtrl.js
+++ scripts/registrationCtrl.js
@@ -1,4 +1,3 @@
 angular.module('myApp').controller('registrationCtrl', ['$scope', 'dataServiceMock', 'stateService', function ($scope, dataServiceMock, stateService) {
 
-
 }]);
