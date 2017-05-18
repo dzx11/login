e42bec8e96ad78f56086d13a3aa26df110e49507
dzeifman <dzeifman2010@gmail.com>
2017-05-18 15:39:13 +0900

    Fix dataServiceMock

--- scripts/dataServiceMock.js
+++ scripts/dataServiceMock.js
@@ -1,7 +1,18 @@
-angular.module('myApp').service('dataServiceMock', function () {
+angular.module('myApp').service('dataServiceMock', ['$q', function ($q) {
     var service = {};
-    
-    
+    service.checkPW = checkPW;
     
     return service;
-});
+    
+    function checkPW (login, pw) {
+        //return a promise with true value if pw correct,
+        //false if pw incorrect
+        var deferred = $q.defer();
+        if (login && pw) {
+            deferred.resolve(true);
+        } else {
+            deferred.resolve(false);
+        }
+        return deferred.promise;
+    }
+}]);
