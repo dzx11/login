9c2afa8bab5d5c2a1dd0f7791bd10abfe8ac92a4
dzeifman <dzeifman2010@gmail.com>
2017-05-18 15:34:20 +0900

    Add registration ctrl code

--- scripts/registrationCtrl.js
+++ scripts/registrationCtrl.js
@@ -1,3 +1,29 @@
-angular.module('myApp').controller('registrationCtrl', ['$scope', 'dataServiceMock', 'stateService', function ($scope, dataServiceMock, stateService) {
-
+angular.module('myApp').controller('registrationCtrl', ['$location', '$log', '$scope', 'dataServiceMock', 'stateService', function ($location, $log, $scope, dataServiceMock, stateService) {
+    
+    //models
+    $scope.login = '';
+    $scope.pw = '';
+    //functions
+    $scope.clickLogin = clickLogin;
+    $scope.clickReset = clickReset;
+    
+    function clickLogin () {
+        if (!$scope.login.length) {
+            //display error message
+        } else {
+            dataServiceMock.checkPW($scope.login, $scope.pw).then(function(isCorrect) {
+                if (isCorrect) {
+                    $location.path('login_success');
+                } else {
+                    //display error message
+                }
+            }).catch(function(error) {
+                $log.error(error);//log the server error
+            });
+        }
+    }
+    
+    function clickReset () {
+        $location.path('pw_reset');//redirect to reset
+    }
 }]);
