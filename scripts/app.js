0cd59a03e7e6d47c411830cbd9184ac360009b54
dzeifman <dzeifman2010@gmail.com>
2017-05-18 15:10:53 +0900

    Fix controller routin

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,20 @@ var myApp = angular.module('myApp', ['ngRoute']);
 myApp.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "templates/login.html"
+        templateUrl : "templates/login.html",
+        controller : "loginCtrl"
     })
     .when("/register", {
-        templateUrl : "templates/register.html"
+        templateUrl : "templates/register.html",
+        controller: "registrationCtrl"
     })
     .when("/pw_reset", {
-        templateUrl : "templates/pw_reset.html"
+        templateUrl : "templates/pw_reset.html",
+        controller: "pwResetCtrl"
     })
     .when("/login_success", {
-        templateUrl : "templates/login_success.html"
+        templateUrl : "templates/login_success.html",
+        controller: "successLoginCtrl",
     })
     .otherwise({
         redirectTo : "/"
