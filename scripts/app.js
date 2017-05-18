11ebed298a19aee9fea614d3cb5f9cd0db986e05
dzeifman <dzeifman2010@gmail.com>
2017-05-18 13:49:43 +0900

    Add routing and template files

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "templates/login.html"
+    })
+    .when("/register", {
+        templateUrl : "templates/register.html"
+    })
+    .when("/pw_reset", {
+        templateUrl : "templates/pw_reset.html"
+    })
+    .when("/login_success", {
+        templateUrl : "templates/login_success.html"
+    })
+    .otherwise({
+        templateUrl : "templates/login.html"
+    });
+});
