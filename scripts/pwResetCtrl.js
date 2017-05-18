4c317329652d77d00ba8ee3ab06ef4faab7400b9
dzeifman <dzeifman2010@gmail.com>
2017-05-18 18:34:15 +0900

    Improve pw reset timers

--- scripts/pwResetCtrl.js
+++ scripts/pwResetCtrl.js
@@ -13,23 +13,24 @@ angular.module('myApp').controller('pwResetCtrl', ['$scope', '$timeout', 'dataSe
     var hasAttemptedReset = false;
     
     function clickReset () {
-        var timer;
+        var timer1, timer2;
         hasAttemptedReset = true;
         if ($scope.login.length > 0) {
             dataServiceMock.checkLoginExists($scope.login).then(function(exists) {
                 $scope.usedLogin = angular.copy($scope.login);
-                $timeout.cancel(timer);//prevent old timers from causing glitches
                 if (exists) {
+                    $timeout.cancel(timer1);//prevent old timers from causing glitches
                     dataServiceMock.sendReset($scope.login);
                     //display success message
                     $scope.showSuccess = true;
-                    timer = $timeout(function () {
+                    timer1 = $timeout(function () {
                         //hide after x seconds
                         $scope.showSuccess = false;
                     }, 4000);
                 } else {
+                    $timeout.cancel(timer2);//prevent old timers from causing glitches
                     $scope.showNotFound = true;
-                    timer = $timeout(function () {
+                    timer2 = $timeout(function () {
                         //hide after x seconds
                         $scope.showNotFound = false;
                     }, 4000);
